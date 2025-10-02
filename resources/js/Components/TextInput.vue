158e0e21c517e0347f54e4fc5f793c5f3f66023b
marcos-sco <marcos_sco@outlook.com>
2025-10-02 16:17:28 -0300

    refactor: replace form inputs with reusable TextInput component for improved consistency

---
+++ resources/js/Components/TextInput.vue
@@ -0,0 +1,40 @@
+<script setup>
+
+const model = defineModel({
+    type: null,
+    required: true,
+});
+
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        default: "text",
+    },
+    message: String
+});
+</script>
+
+<template>
+    <div class="mb-6">
+        <label for="name" class="block mb-2 text-sm font-medium text-gray-700">
+            Name
+        </label>
+
+        <input
+            :type="type"
+            :name="name"
+            :id="name"
+            v-model="model"
+            class="form-input w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
+            :class="{'border-red-600' : message}"
+        />
+
+        <small v-if="message" class="text-red-600"
+            >{{ message }}
+        </small>
+    </div>
+</template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useForm } from "@inertiajs/vue3";
+import TextInput from "../../Components/TextInput.vue";
 
 const form = useForm({
     name: null,
@@ -26,69 +27,42 @@ const submit = () => {
 
     <div class="w-full max-w-md mx-auto bg-white p-8 rounded shadow">
         <form action="" @submit.prevent="submit">
-            <div class="mb-6">
-                <label
-                    for="name"
-                    class="block mb-2 text-sm font-medium text-gray-700"
-                    >Name</label
-                >
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    v-model="form.name"
-                    class="form-input w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
-                <small v-if="form.errors.name" class="text-red-600">{{ form.errors.name }}</small>
-            </div>
 
-            <div class="mb-6">
-                <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-700"
-                    >Email</label
-                >
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    v-model="form.email"
-                    class="form-input w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
-                <small v-if="form.errors.email" class="text-red-600">{{ form.errors.email }}</small>
-            </div>
+            <TextInput
+                name="name"
+                label="Name"
+                type="text"
+                v-model="form.name"
+                :message="form.errors.name"
+                class="mb-6"
+            />
 
-            <div class="mb-6">
-                <label
-                    for="password"
-                    class="block mb-2 text-sm font-medium text-gray-700"
-                    >Password</label
-                >
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    v-model="form.password"
-                    class="form-input w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
-                <small v-if="form.errors.password" class="text-red-600">{{ form.errors.password }}</small>
-            </div>
+            <TextInput
+                name="email"
+                label="Email"
+                type="email"
+                v-model="form.email"
+                :message="form.errors.email"
+                class="mb-6"
+            />
 
-            <div class="mb-6">
-                <label
-                    for="password_confirmation"
-                    class="block mb-2 text-sm font-medium text-gray-700"
-                    >Confirm Password</label
-                >
-                <input
-                    type="password"
-                    name="password_confirmation"
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    class="form-input w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-purple-500"
-                />
-                <small v-if="form.errors.password_confirmation" class="text-red-600">{{ form.errors.password_confirmation }}</small>
-            </div>
+            <TextInput
+                name="password"
+                label="Password"
+                type="password"
+                v-model="form.password"
+                :message="form.errors.password"
+                class="mb-6"
+            />
+
+            <TextInput
+                name="password_confirmation"
+                label="Confirm Password"
+                type="password"
+                v-model="form.password_confirmation"
+                :message="form.errors.password_confirmation"
+                class="mb-6"
+            />
 
             <div>
                 <p class="text-slate-600 mb-2">
@@ -100,7 +74,6 @@ const submit = () => {
                 <button
                     type="submit"
                     class="primary-button w-full bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-700 transition"
-
                     :disabled="form.processing"
                 >
                     Register
